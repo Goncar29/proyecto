b23a78c247048f5a7c6f6bd7320425a15b90fa9c
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2025-07-10 16:46:47 -0300

    Lectura y Escritura de Archivos en Node.js con FS y Path

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config(); // Load environment variables from .env file
-
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, 'users.json');
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies
@@ -49,9 +51,9 @@ app.post('/form', (req, res) => {
 app.post('/api/data', (req, res) => {
     const data = req.body;
 
-    if (!data || Object.keys(data).lenght === 0) {
-        return res.status(400).json({
-            error: `No se recibieron datos`
+    if (err) {
+        return res.status(500).json({
+            error: `Erro con la conexion al servidor`
         });
     }
 
@@ -60,6 +62,30 @@ app.post('/api/data', (req, res) => {
         data: data
     });
 })
+
+app.get('/users', async (req, res) => {
+    try {
+        const data = await fs.promises.readFile(usersFilePath, "utf-8");
+        const users = JSON.parse(data);
+        res.json(users);
+    } catch (err) {
+        console.error("Error al leer users.json:", err);
+        res
+            .status(500)
+            .json({ error: "Error al leer el archivo o archivo corrupto" });
+    }
+    // fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+    //     if (err) {
+    //         return res.status(500).json({
+    //             error: 'Error al leer el archivo de usuarios'
+    //         });
+    //     }
+    //     const user = JSON.parse(data);
+    //     res.json(user);
+    // })
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
