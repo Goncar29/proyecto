83dba64d82df248addb563281f2f717c872ce0cb
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2026-04-16 12:09:10 -0300

    fix(users): apply Joi updateUserSchema validation to PUT /users/:id

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -4,12 +4,15 @@ const reservationsRouter = require('./reservations');
 const appointmentsRouter = require('./appointments');
 const { authenticateToken } = require('../middlewares/auth');
 const auditMiddleware = require('../middlewares/auditMiddleware');
+const validate = require('../middlewares/validate');
+const { updateUserSchema } = require('../schemas/usersSchema');
 
 const router = Router();
 
 router.put(
     '/:id',
     authenticateToken,
+    validate(updateUserSchema),
     auditMiddleware('Actualizar usuario'),
     updateUser
 );
